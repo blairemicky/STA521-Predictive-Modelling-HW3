6bb7f6c8e989766fdd1c5e903a9a81f17567ed85
Merlise Clyde <clyde@stat.duke.edu>
2017-02-02 21:32:42 -0500

    adding wercker files for teams

---
+++ HW3_whitelist.R
@@ -0,0 +1,20 @@
+allowed_files = c("HW3.Rmd",
+                  "HW3.pdf",
+                  "nes5200_processed_voters_realideo.dta",
+                  "README.md",
+                  "HW3_whitelist.R",
+                  "wercker.yml")
+
+files = dir()
+disallowed_files = files[!(files %in% allowed_files)]
+
+if (length(disallowed_files != 0))
+{
+  cat("Disallowed files found:\n")
+  cat("  (remove the following files from your repo)\n\n")
+
+  for(file in disallowed_files)
+    cat("*",file,"\n")
+
+  quit("no",1,FALSE)
+}
